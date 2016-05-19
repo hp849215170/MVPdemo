d11768c0af112cb81635af4d20b4e85463444307
lishoulin <408930131@qq.com>
2016-05-19 10:12:29 +0800

    ...

--- app/src/main/java/demo/lsl/mvpdemo/biz/IUserBiz.java
+++ app/src/main/java/demo/lsl/mvpdemo/biz/IUserBiz.java
@@ -4,6 +4,6 @@ package demo.lsl.mvpdemo.biz;
  * Created by lishoulin on 2016/4/14.
  */
 public interface IUserBiz {
-    public void login(String username, String userpassword, OnLoginListener listener);
+     void login(String username, String userpassword, OnLoginListener listener);
 
 }
